6d09ba628ade0c64146fea1c9163574adac7d05e
k-tateishi <crazyweather0001@gmail.com>
2021-01-19 09:59:44 +0900

    Initial commit

---
+++ src/main/java/com/ktateishi/webnazo/WebnazoApplication.java
@@ -0,0 +1,13 @@
+package com.ktateishi.webnazo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebnazoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebnazoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ktateishi/webnazo/WebnazoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ktateishi.webnazo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebnazoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
